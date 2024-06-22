f5e78d0c4ae2ee675296e37a37e9429dcb399955
2100031431 <bhagyasreeganisetti005@gmail.com>
2024-06-22 20:31:36 +0530

    created Branch

--- tech4humanity/tech4humanity/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def homefunction(request):
-    return render(request,"index.html")
-
-def loginfunction(request):
-    return render(request,"login.html")
